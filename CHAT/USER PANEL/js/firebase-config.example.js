2238dd9ec95711b1dedb7fd6529cf9f1015c8113
vakki-8740 <lightsir8740@gmail.com>
2026-08-28 15:36:50 +0530

    Delete CHAT directory

--- CHAT/USER PANEL/js/firebase-config.example.js
+++
@@ -1,9 +0,0 @@
-var FB_CONFIG = {
-    apiKey: "PASTE_YOUR_API_KEY",
-    authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://PASTE_YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "PASTE_YOUR_PROJECT_ID",
-    storageBucket: "PASTE_YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "PASTE_YOUR_SENDER_ID",
-    appId: "PASTE_YOUR_APP_ID"
-};
